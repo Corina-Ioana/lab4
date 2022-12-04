09c183ef062649a47f75b64e21bced160c7ebea7
Corina-Ioana <corinaioana.dancila@ulbsibiu.ro>
2022-12-04 16:37:32 +0200

    v0.1 commit

---
+++ src/Music.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <v-content>
+      <Search />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Search from "./components/Search.vue";
+
+export default {
+  components: {
+    Search,
+  },
+};
+</script>
+
+<style>
+.application {
+  background-color: #a4508b !important;
+  background-image: linear-gradient(326deg, #a4508b 0%, #5f0a87 74%) !important;
+}
+</style>
