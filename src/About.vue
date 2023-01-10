baeb5c0c77e8a7e0aa0a9a86ae93a3170975817f
Corina-Ioana <corinaioana.dancila@ulbsibiu.ro>
2023-01-10 20:55:01 +0200

    prFinal

--- src/About.vue
+++ src/About.vue
@@ -2,6 +2,16 @@
 
 <template>
   <h1>About</h1>
+  <p class="text-justify">
+ La acest proiect am folosit tehnologia VueJs,la  partea de design am folosit Vuetif.
+Utilizatorul va scrie in bara de cautare numele Artistului. Aplicatia are conceputa o metoda de selectare daca utilizatorul doreste sa caute o melodie anume sau un album.
+ <h3> Detali de implementare </h3>
+ Proiectul contine 3 pagini: Home,Artist, About , care sunt legat intre ele. Am folosit routes pentru a lega paginile. De asemena avem si o componenta Search 
+ in care se afla fech-ul API-ului cu care ma conectez la API-ul primit. Pe baza lui pot cauta artisti dupa nume sau muzica lor.
+
+  </p>
+  
+
 </template>
 
 <style scoped>
--- src/Home.vue
+++ src/Home.vue
@@ -1,11 +1,17 @@
 <script></script>
 
 <template>
-  <h1>Welcome!</h1>
+  <h1 class="font">Welcome!</h1>
+  <v-img src="AviciWall.jpeg" aspect-ratio="1.7"></v-img>
 </template>
 
 <style scoped>
 header {
   line-height: 1.5;
+}
+
+.font {
+  text-align: center;
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
 }
 </style>
